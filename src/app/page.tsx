c51302d97facfdb3251a5d51c7a9919ab8c7c372
Yuvraj Singh <ysgaur9919@gmail.com>
2024-01-15 17:06:52 +0530

    bg color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,7 +28,7 @@ export default async function Home() {
     }
   }
   return (
-    <div className="w-screen min-h-screen bg-gradient-to-r from-rose-100 to-teal-100">
+    <div className="w-screen min-h-screen bg-gradient-to-r from-blue-100 to-purple-100">
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
         <div className="flex flex-col items-center text-center">
           <div className="flex items-center">
